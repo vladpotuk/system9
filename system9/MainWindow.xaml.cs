779c0e67abd3858101fa94c678a9d8303358d78a
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-09 12:31:22 +0300

    update

--- system9/MainWindow.xaml.cs
+++ system9/MainWindow.xaml.cs
@@ -1,24 +1,65 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace system9
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Mutex mutex = new Mutex();
+        private bool firstThreadCompleted = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            FirstThreadOutput.Clear();
+            SecondThreadOutput.Clear();
+            firstThreadCompleted = false;
+
+            Task.Run(() => DisplayAscending());
+            Task.Run(() => DisplayDescending());
+        }
+
+        private void DisplayAscending()
+        {
+            mutex.WaitOne();
+
+            for (int i = 0; i <= 20; i++)
+            {
+                Dispatcher.Invoke(() => FirstThreadOutput.AppendText($"Перший потік: {i}\n"));
+                Thread.Sleep(100);
+            }
+
+            firstThreadCompleted = true;
+            mutex.ReleaseMutex();
+        }
+
+        private void DisplayDescending()
+        {
+            while (true)
+            {
+                mutex.WaitOne();
+
+                if (firstThreadCompleted)
+                {
+                    for (int i = 10; i >= 0; i--)
+                    {
+                        Dispatcher.Invoke(() => SecondThreadOutput.AppendText($"Другий потік: {i}\n"));
+                        Thread.Sleep(100);
+                    }
+
+                    mutex.ReleaseMutex();
+                    break;
+                }
+
+                mutex.ReleaseMutex();
+                Thread.Sleep(100);
+            }
         }
     }
 }

094ee5f127dd0fb74495efa3329a1cb8b0a2274f
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-09 12:58:49 +0300

    update

--- system9/MainWindow.xaml.cs
+++ system9/MainWindow.xaml.cs
@@ -1,24 +1,85 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace system9
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Mutex mutex = new Mutex();
+        private int[] dataArray;
+        private bool isModified = false;
+        private int maxValue = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            ModifiedArrayOutput.Clear();
+            MaxValueOutput.Clear();
+            isModified = false;
+
+            dataArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Task modifyTask = Task.Run(() => ModifyArray());
+            Task findMaxTask = Task.Run(() => FindMaxValue());
+
+            Task.WhenAll(modifyTask, findMaxTask).ContinueWith(t =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    ModifiedArrayOutput.AppendText("Модифікований масив:\n");
+                    foreach (var item in dataArray)
+                    {
+                        ModifiedArrayOutput.AppendText(item + "\n");
+                    }
+                    MaxValueOutput.Text = $"Максимальне значення в масиві: {maxValue}";
+                });
+            });
+        }
+
+        private void ModifyArray()
+        {
+            Random random = new Random();
+            mutex.WaitOne();
+
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                int randomValue = random.Next(1, 10);
+                dataArray[i] += randomValue;
+            }
+
+            isModified = true;
+            mutex.ReleaseMutex();
+        }
+
+        private void FindMaxValue()
+        {
+            while (true)
+            {
+                mutex.WaitOne();
+
+                if (isModified)
+                {
+                    maxValue = dataArray[0];
+                    for (int i = 1; i < dataArray.Length; i++)
+                    {
+                        if (dataArray[i] > maxValue)
+                        {
+                            maxValue = dataArray[i];
+                        }
+                    }
+                    mutex.ReleaseMutex();
+                    break;
+                }
+
+                mutex.ReleaseMutex();
+                Thread.Sleep(100);
+            }
         }
     }
 }
